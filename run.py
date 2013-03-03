19277e06c1ca43ddd5444bf21c496840d7fb38b5
Alok Gandhi <alok.gandhi2002@gmail.com>
2013-03-03 18:14:14 -0500

    changed version info in license text

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
-####################################################################################
-####################################################################################
-##                                                                                ##
-##  Scenetoc Resolution Manager (c) 2013 Alok Gandhi (alok.gandhi2002@gmail.com)  ##
-##                                                                                ##
-##                                                                                ##
-##  This program is free software: you can redistribute it and/or modify it       ##
-##  under the terms of the GNU General Public License, Version 3, 29 June 2007    ##
-##  as published by the Free Software Foundation   <http://fsf.org/>              ##
-##  TO view the full license, read license.txt in the folder ../license           ##
-##                                                                                ##
-####################################################################################
-####################################################################################
+##########################################################################################
+##########################################################################################
+##                                                                                      ##
+##  Scenetoc Resolution Manager V 1.0 (c) 2013 Alok Gandhi (alok.gandhi2002@gmail.com)  ##
+##                                                                                      ##
+##                                                                                      ##
+##  This program is free software: you can redistribute it and/or modify it             ##
+##  under the terms of the GNU General Public License, Version 3, 29 June 2007          ##
+##  as published by the Free Software Foundation   <http://fsf.org/>                    ##
+##  TO view the full license, read license.txt in the folder ../license                 ##
+##                                                                                      ##
+##########################################################################################
+##########################################################################################
 
 if __name__ == "__main__":
     from mainWindow import run
