ad03480b03ec101c5afec1dcb6f2b75e2f757e13
alok.gandhi <alok.gandhi@modusfx.com>
2013-03-15 15:29:05 -0400

    updated license info in files

--- msgHandler.py
+++ msgHandler.py
@@ -1,16 +1,25 @@
-##########################################################################################
-##########################################################################################
-##                                                                                      ##
+###########################################################################################
+###########################################################################################
+##                                                                                       ##
 ##  Scenetoc Resolution Manager V 1.02 (c) 2013 Alok Gandhi (alok.gandhi2002@gmail.com)  ##
-##                                                                                      ##
-##                                                                                      ##
-##  This program is free software: you can redistribute it and/or modify it             ##
-##  under the terms of the GNU General Public License, Version 3, 29 June 2007          ##
-##  as published by the Free Software Foundation   <http://fsf.org/>                    ##
-##  TO view the full license, read license.txt in the folder ../license                 ##
-##                                                                                      ##
-##########################################################################################
-##########################################################################################
+##                                                                                       ##
+##                                                                                       ##
+##  This file is part of Scenetoc Res Manager.                                           ##
+##                                                                                       ##
+##  Scenetoc Res Manager is free software: you can redistribute it and/or modify         ##
+##  it under the terms of the GNU General Public License, Version 3, 29 June 2007        ##
+##  as published by the Free Software Foundation,                                        ##
+##                                                                                       ##
+##  Scenetoc Res Manager is distributed in the hope that it will be useful,              ##
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of                       ##
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                        ##
+##  GNU General Public License for more details.                                         ##
+##                                                                                       ##
+##  You should have received a copy of the GNU General Public License                    ##
+##  along with Scenetoc Res Manager.  If not, see <http://www.gnu.org/licenses/>.        ##
+##                                                                                       ##
+###########################################################################################
+###########################################################################################
 
 import os
 import sys
